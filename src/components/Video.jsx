96c8dc7a7687a14d802a7f96b39bb03fe256851a
miguebauducco <miguel.bau14@gmail.com>
2025-08-22 00:12:43 -0300

    mmm

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -14,7 +14,7 @@ function Video() {
     controlsList="nodownload nofullscreen noremoteplayback noplaybackrate"
     preload="metadata"
     disablePictureInPicture
-    src="/videos/HORIZONTAL_C.mp4"
+    src="/diabetes-recovery/videos/HORIZONTAL_C.mp4"
     />
 
     <video
@@ -27,7 +27,7 @@ function Video() {
     controlsList="nodownload nofullscreen noremoteplayback noplaybackrate"
     preload="metadata"
     disablePictureInPicture
-    src="/videos/VERTICAL_C.mp4"
+    src="/diabetes-recovery/videos/VERTICAL_C.mp4"
     />
     </div>
 
