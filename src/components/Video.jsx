30a1612f67a2ba45e23bbdf12ce60fd0814988da
miguebauducco <miguel.bau14@gmail.com>
2025-08-22 01:26:15 -0300

    videos en funcionamiento

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -3,12 +3,21 @@ import React from 'react';
 
 function Video() {
   return (
-    <div className="video-container">
+    <div className="video-container"  style={{ borderRadius: "30px", overflow: "hidden" }}>
       <iframe 
+      className="videoDesktop"
       allow="fullscreen;autoplay" 
       allowfullscreen
       src="https://streamable.com/e/cbvxmn?autoplay=1&muted=1"
-      style={{borderRadius:"30px", width:"100%", height:"100%", overflow:"hidden"}}>
+      style={{borderRadius:"30px", width:"100%", height:"100%", overflow:"hidden", border:"none"}}>
+      </iframe>
+
+      <iframe
+      className="videoMobile"
+      allow="fullscreen;autoplay"
+      allowfullscreen
+      src="https://streamable.com/e/rjvo28?autoplay=1&muted=1"
+      style={{borderRadius:"30px", width:"95%", height:"95%", overflow:"hidden", border:"none"}}>
       </iframe>
 
     </div>
