7069eb4e14103ed997872492cc8a0f4ed12b8e9d
miguebauducco <miguel.bau14@gmail.com>
2025-08-22 14:58:23 -0300

    fixing mobile

--- src/components/HeroMensaje.jsx
+++ src/components/HeroMensaje.jsx
@@ -2,11 +2,19 @@ import React from 'react'
 
 export const HeroMessage = () => {
   return (
-    <div className="hero-message">
-      <p className="text1">Hecho por alguien que te entiende</p>
-      <p className="text2">Acompañarte es parte del plan</p>
+    <div className='hero-section'>
+      <div className="hero-message">
+        <p className="text1">Hecho por alguien que te entiende</p>
+        <p className="text2">Acompañarte es parte del plan</p>
+      </div>
+      <div className="hero-message-mobile">
+        <p className="text1">Hecho por alguien<br /> que te entiende</p>
+        <p className="text2">Acompañarte<br /> es parte del plan</p>
+      </div>
     </div>
   )
 }
+
+
 
 export default HeroMessage;
