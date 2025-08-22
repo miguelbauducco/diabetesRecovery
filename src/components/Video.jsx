d6121fa20537f0d7cac8d88a921ee3ef97a0b6da
miguebauducco <miguel.bau14@gmail.com>
2025-08-22 13:35:26 -0300

    mejoras

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,27 +1,66 @@
 import React from 'react';
 
-
 function Video() {
   return (
-    <div className="video-container"  style={{ borderRadius: "30px", overflow: "hidden" }}>
-      <iframe 
-      className="videoDesktop"
-      allow="fullscreen;autoplay" 
-      allowfullscreen
-      src="https://streamable.com/e/cbvxmn?autoplay=1&muted=1"
-      style={{borderRadius:"30px", width:"100%", height:"100%", overflow:"hidden", border:"none"}}>
-      </iframe>
+    <div
+      className="video-container"
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        marginTop: '30px',
+        backgroundColor: 'transparent'
+      }}
+    >
+      {/* Desktop video */}
+      <div
+        className="videoDesktop"
+        style={{
+          borderRadius: '30px',
+          overflow: 'hidden',
+          width: '100%',
+          height: '100%',
+        }}
+      >
+        <iframe
+          allow="fullscreen;autoplay"
+          allowFullScreen
+          src="https://streamable.com/e/cbvxmn?autoplay=1&muted=1"
+          style={{
+            width: '100%',
+            height: '100%',
+            border: 'none',
+            display: 'block',
+          }}
+        />
+      </div>
 
-      <iframe
-      className="videoMobile"
-      allow="fullscreen;autoplay"
-      allowfullscreen
-      src="https://streamable.com/e/rjvo28?autoplay=1&muted=1"
-      style={{borderRadius:"30px", width:"95%", height:"95%", overflow:"hidden", border:"none"}}>
-      </iframe>
-
+      {/* Mobile video */}
+      <div
+        className="videoMobile"
+        style={{
+          borderRadius: '30px',
+          overflow: 'hidden',
+          width: '100%',
+          maxWidth: '400px',
+          height: '700px',
+          marginTop:"30px",
+          boxShadow: '0 0 20px rgba(0, 0, 0, 0.1)',
+        }}
+      >
+        <iframe
+          allow="fullscreen;autoplay"
+          allowFullScreen
+          src="https://streamable.com/e/rjvo28?autoplay=1&muted=1"
+          style={{
+            width: '100%',
+            height: '100%',
+            display: 'block',
+            border: 'none',
+          }}
+        />
+      </div>
     </div>
-
   );
 }
 
