08e84cc76b2c15b531e9c1e73eaf61f9178c8af6
miguelbauducco <miguel.bau14@gmail.com>
2025-08-25 20:35:45 -0300

    ajustes

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -2,60 +2,22 @@ import React from 'react';
 
 function Video() {
   return (
-    <div
-      className="video-container"
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        marginTop: '30px',
-        backgroundColor: 'transparent'
-      }}
-    >
+    <div  className="video-container">
       {/* Desktop video */}
-      <div
-        className="videoDesktop"
-        style={{
-          borderRadius: '30px',
-          overflow: 'hidden',
-          width: '100%',
-          height: '100%',
-        }}
-      >
+      <div className="videoDesktop">
         <iframe
           allow="fullscreen;autoplay"
           allowFullScreen
           src="https://streamable.com/e/cbvxmn?autoplay=1&muted=1"
-          style={{
-            width: '100%',
-            height: '100%',
-            border: 'none',
-            display: 'block',
-          }}
         />
       </div>
 
       {/* Mobile video */}
-      <div
-        className="videoMobile"
-        style={{
-          borderRadius: '30px',
-          overflow: 'hidden',
-          width: '100%',
-          height:'570px',
-          boxShadow: '0 0 20px rgba(0, 0, 0, 0.1)',
-        }}
-      >
+      <div className="videoMobile">
         <iframe
           allow="fullscreen;autoplay"
           allowFullScreen
           src="https://streamable.com/e/rjvo28?autoplay=1&muted=1"
-          style={{
-            width: '100%',
-            height: '700px',
-            display: 'block',
-            border: 'none',
-          }}
         />
       </div>
     </div>
