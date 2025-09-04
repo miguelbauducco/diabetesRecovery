68e12bddaf987f5d1a01e59a4f4a80baf64ba9a3
miguelbauducco <miguel.bau14@gmail.com>
2025-09-03 21:06:38 -0300

    changes

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -3,16 +3,18 @@ import React from 'react';
 function Video() {
   return (
     <div  className="video-container">
-    <iframe className="videoDesktop"src="https://www.youtube.com/embed/bq6xatlMxPo" title="Put Type 2 Diabetes In Remission" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-    
-          <div className="videoMobile">
+     <iframe className="videoDesktop"src="https://www.youtube.com/embed/bq6xatlMxPo" title="Put Type 2 Diabetes In Remission" 
+        frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+        referrerpolicy="strict-origin-when-cross-origin" 
+        allowfullscreen>
+     </iframe>
+
+      <div className="videoMobile">
         <iframe
           src="https://streamable.com/e/rjvo28?autoplay=1&muted=1"
         />
       </div>
-    
-    
-    
     </div>
 
   );
